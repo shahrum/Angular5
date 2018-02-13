ac670c5328f83a86f167e3baaf95c365ad189fa5
shha268@gmail.com <shha268@gmail.com>
2018-02-13 18:59:30 +1100

    Event Binding
    very easy just pay attention to comments in the code

--- angular5/src/app/home/home.component.ts
+++ angular5/src/app/home/home.component.ts
@@ -6,13 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-	itemCount:number=4;
-	btnText:string = "Add An Item";
-	goalText:string = "My first life goal";
+	itemCount : number;
+	btnText : string = "Add An Item";
+	goalText : string = "My first life goal";
+	goals = [];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  addItem(){
+  	//add items to goals array
+  	this.goals.push(this.goalText);
+
+  	//clear the goalText field
+  	this.goalText="";
+  	
+  	//read the goals array lenth and assign it itemCount
+  	this.itemCount=this.goals.length;
   }
 
 }
