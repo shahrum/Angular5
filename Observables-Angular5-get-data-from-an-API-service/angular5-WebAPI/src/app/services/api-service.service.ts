6f1eb0f2bab05cc0f9f816d858013f992d16b697
Shahram Hosseinabadi <shha268@gmail.com>
2018-03-18 13:09:39 +1100

    ApiService

---
+++ Observables-Angular5-get-data-from-an-API-service/angular5-WebAPI/src/app/services/api-service.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { Http,Response } from '@angular/http';
+import { Observable } from 'rxjs';
+import 'rxjs/Rx';
+import { Iposts } from '../interface/iposts';
+
+@Injectable()
+export class ApiService {
+  private _postsUrl = "https://jsonplaceholder.typicode.com/posts";
+
+
+  constructor(private http:Http) { 
+
+  }
+
+  getPosts():Observable<Iposts>{
+    return this.http
+               .get(this._postsUrl)
+               .map((res:Response)=>{
+                 return <Iposts[]>res.json()
+               })
+               .catch(this.handleError);
+  }
+  
+  private handleError(error:Response){
+    return Observable.throw(error.status);
+  } 
+
+
+}
